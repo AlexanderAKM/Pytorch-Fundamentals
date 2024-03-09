ff208009c09173732d846cd15155d58c4ffb632d
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-03-09 19:50:54 +0100

    Can completely train a model with terminal

--- going_modular/train.py
+++ going_modular/train.py
@@ -1,4 +1,4 @@
-def main():
+def main(learning_rate: float, hidden_units: int, batch_size: int, num_epochs: int):
   """
   Trains a PyTorch image classification model using device-agnostic code.
   """
@@ -8,12 +8,6 @@ def main():
   import data_setup, engine, model_builder, utils
 
   from torchvision import transforms
-
-  # Setup hyperparameters
-  NUM_EPOCHS = 5
-  BATCH_SIZE = 32
-  HIDDEN_UNITS = 10
-  LEARNING_RATE = 0.001
 
   # Setup directories
   train_dir = "data/pizza_steak_sushi/train"
@@ -33,20 +27,20 @@ def main():
       train_dir=train_dir,
       test_dir=test_dir,
       transform=data_transform,
-      batch_size=BATCH_SIZE
+      batch_size=batch_size
   )
 
   # Create model with help from model_builder.py
   model = model_builder.TinyVGG(
       input_shape=3,
-      hidden_units=HIDDEN_UNITS,
+      hidden_units=hidden_units,
       output_shape=len(class_names)
   ).to(device)
 
   # Set loss and optimizer
   loss_fn = torch.nn.CrossEntropyLoss()
   optimizer = torch.optim.Adam(model.parameters(),
-                              lr=LEARNING_RATE)
+                              lr=learning_rate)
 
   # Start training with help from engine.py
   engine.train(model=model,
@@ -54,13 +48,22 @@ def main():
               test_dataloader=test_dataloader,
               loss_fn=loss_fn,
               optimizer=optimizer,
-              epochs=NUM_EPOCHS,
+              epochs=num_epochs,
               device=device)
 
   # Save the model with help from utils.py
   utils.save_model(model=model,
                   target_dir="models",
-                  model_name="05_going_modular_script_mode_tinyvgg_model.pth")
+                  model_name="05_going_modular_script_mode_tinyvgg_model_" + f"{learning_rate}_{batch_size}_{num_epochs}_hidden_units_" + ".pth")
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description='Hyperparameters for the model')
+    parser.add_argument('--learning_rate', type=float, default=0.001, help='The learning rate for the model')
+    parser.add_argument('--batch_size', type=int, default=32, help='The batch_size for training')
+    parser.add_argument('--num_epochs', type=int, default=5, help='The number of epochs to run the model')
+    parser.add_argument('--hidden_units', type=int, default=10, help='The number of hidden units')
+
+    args = parser.parse_args()
+
+    main(args.learning_rate, args.hidden_units, args.batch_size, args.num_epochs)
